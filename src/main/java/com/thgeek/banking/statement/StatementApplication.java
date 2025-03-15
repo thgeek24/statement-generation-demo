159a4f68fe0f5d930b72bfae5014ad49db9d9bd1
THGeek <alex24ht@hotmail>
2025-03-15 22:03:05 +0800

    feat: initial commit

---
+++ src/main/java/com/thgeek/banking/statement/StatementApplication.java
@@ -0,0 +1,13 @@
+package com.thgeek.banking.statement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatementApplication.class, args);
+    }
+
+}
